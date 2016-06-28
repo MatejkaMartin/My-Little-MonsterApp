6639d5929e6d2ac68658446840352477f4921cff
Martin Prace <martinmatejka@martin-macbook-pro.local>
2016-06-28 14:47:13 +0200

    Initial Commit

---
+++ FINAL-LITTLE-MONSTER/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FINAL-LITTLE-MONSTER
+//
+//  Created by Martin Matějka on 28.06.16.
+//  Copyright © 2016 Martin Matějka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
